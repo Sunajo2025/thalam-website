08ebd66f1fca8d5af45453c6108ac79a0bfe6fd7
Gokul <gokulsusela@gmail.com>
2026-02-09 00:24:53 +0530

    project-completed

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./js/*.js"],
+  theme: {
+  extend: {
+    fontFamily: {
+      heading: ["Gantari", "sans-serif"],
+      body: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      primary: "#262626",
+      background: "#F7F7F2",
+      softPurple: "#DEDBE6",
+      softBeige: "#EAE3D9",
+      white: "#FFFFFF",
+    },
+  },
+},
+
+  plugins: [],
+}
+
